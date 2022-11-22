451c8b45d5a776c7a0f58798b89613bf5de752df
lhuerta13 <lucas.huerta@edu.univ-fcomte.fr>
2022-11-22 16:30:51 +0100

    [ADD] Entities, test Login

---
+++ symfony_project/src/Entity/Chat.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChatRepository::class)]
+class Chat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $topic = null;
+
+    #[ORM\OneToMany(mappedBy: 'Chat', targetEntity: Messages::class)]
+    private Collection $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTopic(): ?int
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(int $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Messages>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Messages $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages->add($message);
+            $message->setChat($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Messages $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getChat() === $this) {
+                $message->setChat(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ symfony_project/src/Entity/Messages.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessagesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessagesRepository::class)]
+class Messages
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToMany(mappedBy: 'messages', targetEntity: User::class)]
+    private Collection $User;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    private ?Chat $Chat = null;
+
+    public function __construct()
+    {
+        $this->User = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUser(): Collection
+    {
+        return $this->User;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->User->contains($user)) {
+            $this->User->add($user);
+            $user->setMessages($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->User->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getMessages() === $this) {
+                $user->setMessages(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getChat(): ?Chat
+    {
+        return $this->Chat;
+    }
+
+    public function setChat(?Chat $Chat): self
+    {
+        $this->Chat = $Chat;
+
+        return $this;
+    }
+}
