769439d4d107a1a6e460c82b2ccd02b20dad010a
Charles <charlesmangin.98@gmail.com>
2022-11-24 14:40:13 +0000

    Password hash

--- symfony_project/src/Factory/UserFactory.php
+++ symfony_project/src/Factory/UserFactory.php
@@ -43,7 +43,7 @@ final class UserFactory extends ModelFactory
     {
         return [
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
-            'username' => self::faker()->text(),
+            'username' => self::faker()->firstName,
             'roles' => []
         ];
     }
